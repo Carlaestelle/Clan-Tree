f136fb56a85571948b9bb3187d89117155b521d1
Administrator <carlamwasumbi28@gmail.com>
2026-09-26 23:48:22 +0300

    Second part of fix, hope this works

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    const token = req.nextauth.token;
+    const isChangePasswordPage = req.nextUrl.pathname === "/change-password";
+
+    if (token?.mustChangePassword && !isChangePasswordPage) {
+      return NextResponse.redirect(new URL("/change-password", req.url));
+    }
+
+    return NextResponse.next();
+  },
+  {
+    pages: {
+      signIn: "/login",
+    },
+  }
+);
+
+export const config = {
+  matcher: [
+    "/((?!login|api/auth|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
